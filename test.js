882e28724a52b82f925f2142bcc15a82ddc3845d
Azz-eddine BERRAMOU <azedine.berramou@gmail.com>
2021-04-29 12:12:53 +0200

    Fix failed test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import strToMachineName from '.';
 
 test('main', t => {
-	t.is(strToMachineName('', ''), '-');
+	t.is(strToMachineName('', '-'), '');
 	t.is(strToMachineName('C\'est une phrase française avec des accenté éàçàçéìù', '_'), 'cest_une_phrase_francaise_avec_des_accente_eacaceiu');
 	t.is(strToMachineName('C\'est une phrase française avec des accenté éàçàçéìù', '-'), 'cest-une-phrase-francaise-avec-des-accente-eacaceiu');
 	t.is(strToMachineName('C\'est une phrase française avec des accenté éàçàçéìù', '/'), 'cest/une/phrase/francaise/avec/des/accente/eacaceiu');
